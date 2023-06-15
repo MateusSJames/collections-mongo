ccecb5867ccbd3873e9a009fb8a907e18e567bb3
MateusSJames <mateusaristides23@gmail.com>
2023-06-15 14:08:06 -0300

    feat: add function for multiples connect strings

--- main.go
+++ main.go
@@ -15,9 +15,19 @@ func main() {
 	service.ConnectDb()
 	dates, err := service.FindClassByStudent("648a1422c875a5d4d496134d")
 
+	dates2, err2 := service.ConnectWithOnlyDB("648a1422c875a5d4d496134d")
+
 	if(err != nil) {
 		fmt.Println(err)
 	}
 
+	if(err2 != nil) {
+		fmt.Println(err2)
+	}
+	fmt.Println("----- Conexao com 1 connection string -----")
 	fmt.Println(dates)
+	fmt.Println("-------------------------------------------")
+	fmt.Println("----- Conexao com 2 connection strings -----")
+	fmt.Println(dates2)
+	fmt.Println("-------------------------------------------")
 }
--- service/mongo.go
+++ service/mongo.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"log"
 	"os"
 	"time"
 
@@ -53,6 +54,51 @@ func FindClassByStudent(alunoID string) (bson.M, error) {
 	turmaProjection := bson.M{"turma": 1, "turno": 1}
     var turma bson.M
     if err := collectionClass.FindOne(ctx, turmaFilter, options.FindOne().SetProjection(turmaProjection)).Decode(&turma); err != nil {
+        return nil, err
+    }
+
+	aluno["turma"] = turma
+
+	return aluno, nil
+}
+
+//Exemplo para duas connection string
+func ConnectWithOnlyDB(alunoID string) (bson.M, error) {
+	// Conexão com o primeiro banco de dados
+	connStrDb1 := os.Getenv("URI_DB1")
+	connStrDb2 := os.Getenv("URI_DB2")
+	client1, err := mongo.Connect(context.Background(), options.Client().ApplyURI(connStrDb1))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client1.Disconnect(context.Background())
+
+	// Conexão com o segundo banco de dados
+	client2, err := mongo.Connect(context.Background(), options.Client().ApplyURI(connStrDb2))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client2.Disconnect(context.Background())
+
+	// Exemplo de uso das conexões
+	db1 := client1.Database("banco1").Collection("usuarios")
+	db2 := client1.Database("banco2").Collection("turma-example")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	
+	// Consulta para buscar o aluno pelo ID
+    alunoFilter := bson.M{"_id": alunoID}
+    var aluno bson.M
+    if err := db1.FindOne(ctx, alunoFilter).Decode(&aluno); err != nil {
+        return nil, err
+    }
+
+    // Consulta para buscar a turma do aluno
+    turmaFilter := bson.M{"id_aluno": alunoID}
+	turmaProjection := bson.M{"turma": 1, "turno": 1}
+    var turma bson.M
+    if err := db2.FindOne(ctx, turmaFilter, options.FindOne().SetProjection(turmaProjection)).Decode(&turma); err != nil {
         return nil, err
     }
 
